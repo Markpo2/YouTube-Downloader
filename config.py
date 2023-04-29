193ffa75474c63e5fec3e9a4497f298f23b3df18
Markpo2 <127012533+Markpo2@users.noreply.github.com>
2023-04-29 02:09:44 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("7143337", ))
-    API_HASH = os.getenv("1afa55a5f3bf7058c843d1b290f79c49", '')
-    BOT_TOKEN = os.getenv("6043283784:AAHLV11M9g3gaDj5dE-Sr5fKhSCd8CT-lOc", '')
+    API_ID = int(os.getenv("API_ID", 7143337))
+    API_HASH = os.getenv("API_HASH", '1afa55a5f3bf7058c843d1b290f79c49')
+    BOT_TOKEN = os.getenv("BOT_TOKEN", '6043283784:AAHLV11M9g3gaDj5dE-Sr5fKhSCd8CT-lOc')
